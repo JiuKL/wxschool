4aa3e863bc7956a55a047d8ddc36951055f172f2
9k <1226630618@qq.com>
2020-08-04 19:47:37 +0800

    首页部分完成

---
+++ miniprogram/components/m_select/m_select.js
@@ -0,0 +1,24 @@
+// Componet/Componet.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+    properties: {
+        name:{
+            type:String,
+            value:''
+        }
+    },
+  /**
+   * 组件的初始数据
+   */
+    data: {
+        nowText:"理工",//初始内容
+    },
+  /**
+   * 组件的方法列表
+   */
+    methods: {
+　　　
+    }
+})
